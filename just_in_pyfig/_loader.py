ae58efcd93aa257c08c7a6a56012bf6a6a8fa784
Justin <justin.gray@reeldata.ai>
2024-07-17 00:25:36 -0300

    extract into a different module

--- just_in_pyfig/_loader.py
+++ just_in_pyfig/_loader.py
@@ -1,57 +1,10 @@
 from typing import Type, TypeVar, Dict, Iterable
 
 from ._pyfig import Pyfig
+from ._override import unify_overrides, apply_overrides
 
 
 T = TypeVar("T", bound=Pyfig)
-
-def _unify_overrides(*overrides: Dict) -> Dict:
-    """
-    Configuration overrides are unified by merging them together at a dictionary-key level. This means that if a key
-    is present in multiple overrides, the last one will take precedence. Overrides are always performed at the lowest
-    dictionary level possible.
-
-    Args:
-        overrides: descending order of precedence
-
-    Returns:
-        the unified override dictionary
-    """
-    unified = {}
-
-    for override in reversed(overrides):
-        for key, value in override.items():
-            if key in unified and isinstance(value, dict) and isinstance(unified[key], dict):
-                unified[key] = _unify_overrides(value, unified[key])
-            else:
-                unified[key] = value
-
-    return unified
-
-
-def _apply_override_to_conf(conf: Dict, override: Dict, trace: str="") -> None:
-    """
-    Using a base configuration, applies an override to it.
-
-    The override is applied at the lowest possible dictionary-key level.
-
-    Args:
-        conf:       the base configuration
-        override:   the override to apply
-        trace:      the current trace of the configuration
-
-    Returns:
-        None (mutates `conf` in place)
-    """
-    for key, value in override.items():
-        if key not in conf:
-            # TODO: consider logging a warning instead depending on some argument
-            raise KeyError(f"Unknown key '{key}' in override ({trace or 'root'})")
-
-        if isinstance(value, dict):
-            _apply_override_to_conf(conf[key], value, trace=f"{trace}.{key}")
-        else:
-            conf[key] = value
 
 
 def load_configuration(default: Type[T], overrides: Iterable[Dict]) -> T:
@@ -59,8 +12,8 @@ def load_configuration(default: Type[T], overrides: Iterable[Dict]) -> T:
     TODO
     """
     conf = default().model_dump() # TODO: consider using 'serialize_as_any' kwarg
-    unified_overrides = _unify_overrides(*list(overrides))
-    _apply_override_to_conf(conf, unified_overrides)
+    unified_overrides = unify_overrides(*list(overrides))
+    apply_overrides(conf, unified_overrides)
 
     # evaluate ??
 
---
+++ just_in_pyfig/_override.py
@@ -0,0 +1,50 @@
+from typing import Dict
+
+
+def unify_overrides(*overrides: Dict) -> Dict:
+    """
+    Configuration overrides are unified by merging them together at a dictionary-key level. This means that if a key
+    is present in multiple overrides, the last one will take precedence. Overrides are always performed at the lowest
+    dictionary level possible.
+
+    Args:
+        overrides: descending order of precedence
+
+    Returns:
+        the unified override dictionary
+    """
+    unified = {}
+
+    for override in reversed(overrides):
+        for key, value in override.items():
+            if key in unified and isinstance(value, dict) and isinstance(unified[key], dict):
+                unified[key] = unify_overrides(value, unified[key])
+            else:
+                unified[key] = value
+
+    return unified
+
+
+def apply_overrides(conf: Dict, override: Dict, trace: str="") -> None:
+    """
+    Using a base configuration, applies an override to it.
+
+    The override is applied at the lowest possible dictionary-key level.
+
+    Args:
+        conf:       the base configuration
+        override:   the override to apply
+        trace:      the current trace of the configuration
+
+    Returns:
+        None (mutates `conf` in place)
+    """
+    for key, value in override.items():
+        if key not in conf:
+            # TODO: consider logging a warning instead depending on some argument
+            raise KeyError(f"Unknown key '{key}' in override ({trace or 'root'})")
+
+        if isinstance(value, dict):
+            apply_overrides(conf[key], value, trace=f"{trace}.{key}")
+        else:
+            conf[key] = value
