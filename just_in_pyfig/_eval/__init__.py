892b1663c389642c77da11e4cb44b277ba950281
Justin <justin.gray@reeldata.ai>
2024-07-17 14:54:37 -0300

    -

---
+++ just_in_pyfig/_eval/__init__.py
@@ -0,0 +1,4 @@
+from .evaluate_conf import evaluate_conf
+from .abstract_evaluator import AbstractEvaluator
+from .variable_evaluator import VariableEvaluator
+from .environment_evaluator import EnvironmentEvaluator
--- just_in_pyfig/_loader.py
+++ just_in_pyfig/_loader.py
@@ -1,20 +1,19 @@
-from typing import Type, TypeVar, Dict, Iterable
+from typing import Type, TypeVar, Dict, Iterable, Collection
 
 from ._pyfig import Pyfig
 from ._override import unify_overrides, apply_overrides
+from ._eval import evaluate_conf, AbstractEvaluator
 
 
 T = TypeVar("T", bound=Pyfig)
 
 
-def load_configuration(default: Type[T], overrides: Iterable[Dict]) -> T:
+def load_configuration(default: Type[T], overrides: Iterable[Dict], evaluators: Collection[AbstractEvaluator]) -> T:
     """
     TODO
     """
     conf = default().model_dump() # TODO: consider using 'serialize_as_any' kwarg
     unified_overrides = unify_overrides(*list(overrides))
     apply_overrides(conf, unified_overrides)
-
-    # evaluate ??
-
+    evaluate_conf(conf, evaluators)
     return default(**conf)
