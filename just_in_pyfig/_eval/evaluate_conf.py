11c74064f3128e4c70542a9b8c44000ae3937cf1
Justin <justin.gray@reeldata.ai>
2024-07-17 13:09:34 -0300

    docs

--- just_in_pyfig/_eval/evaluate_conf.py
+++ just_in_pyfig/_eval/evaluate_conf.py
@@ -5,6 +5,19 @@ from .abstract_evaluator import AbstractEvaluator
 
 
 def _find_evaluator(name: str, evaluators: Collection[AbstractEvaluator]) -> AbstractEvaluator:
+    """
+    Finds an evaluator by name in a collection of evaluators.
+
+    Args:
+        name:       the name of the evaluator to find
+        evaluators: the collection of evaluators to search through
+
+    Returns:
+        The evaluator with the given name.
+
+    Raises:
+        ValueError if no evaluator is found or if multiple evaluators are found with the same name.
+    """
     candidate: Optional[AbstractEvaluator] = None
 
     for evaluator in evaluators:
@@ -21,9 +34,33 @@ def _find_evaluator(name: str, evaluators: Collection[AbstractEvaluator]) -> Abs
 
 
 _TEMPLATE_PATTERN = re.compile(r"\$\{(?P<evaluator>[A-Za-z0-9_-]*)(\.(?P<value>[^}]*))?\}")
+"""
+A regular expression pattern that matches a template string.
+
+A template (sub)string starts with a dollar sign ($) and is wrapped in curly braces ({}). It consists of two parts:
+1. The evaluator name, which is a sequence of alphanumeric characters, underscores, and hyphens.
+2. The value, which is an optional sequence of characters that is separated from the evaluator name by a period.
+   Note: the value can contain anything except double closing curly braces (}}). FIXME not true yet
+
+E.g.,
+    "${foo}" -> { "evaluator": "foo", "value": None }
+    "${foo.bar}" -> { "evaluator": "foo", "value": "bar" }
+    "some ${eval.val} string" -> { "evaluator": "eval", "value": "val" }
+"""
 
 
 def _evaluate_string(string: str, evaluators: Collection[AbstractEvaluator]) -> Any:
+    """
+    Given a string, evaluates all templates in the string using the provided evaluators.
+
+    Args:
+        string:      the string to evaluate
+        evaluators:  the collection of evaluators to use
+
+    Returns:
+        the modified string with all templates evaluated
+        if the template is the entire string, then the type of the evaluator's return value is kept
+    """
     # if the entire string is a template, evaluate it and keep the type
     if full_match := _TEMPLATE_PATTERN.fullmatch(string):
         evaluator = _find_evaluator(full_match.group("evaluator"), evaluators)
@@ -38,6 +75,9 @@ def _evaluate_string(string: str, evaluators: Collection[AbstractEvaluator]) -> 
 
 
 def evaluate_conf(conf: Union[list, dict], evaluators: Collection[AbstractEvaluator]) -> None:
+    """
+    TODO
+    """
     changes = 1
 
     while changes > 0:
