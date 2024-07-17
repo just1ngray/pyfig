b5d936f7244eeb6c632a53edef16cac8b36762fe
Justin <justin.gray@reeldata.ai>
2024-07-17 11:43:04 -0300

    variable evaluator

---
+++ just_in_pyfig/_eval/variable_evaluator.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from .abstract_evaluator import AbstractEvaluator, EvaluationError
+
+
+class VariableEvaluator(AbstractEvaluator):
+    """
+    A simple evaluator which enables variable interpolation for values known at initialization time.
+    """
+    def __init__(self, **variables: Any):
+        self._variables = variables
+
+    def name(self) -> str:
+        return "var"
+
+    def evaluate(self, value: str) -> Any:
+        try:
+            return self._variables[value]
+        except KeyError as exc:
+            raise EvaluationError(self, value) from exc
---
+++ just_in_pyfig/_eval/variable_evaluator_test.py
@@ -0,0 +1,23 @@
+import pytest
+
+from .abstract_evaluator import EvaluationError
+from .variable_evaluator import VariableEvaluator
+
+
+def test__given_empty_variable_evaluator__when_evaluate__then_raises_evaluation_error():
+    evaluator = VariableEvaluator()
+    with pytest.raises(EvaluationError):
+        evaluator.evaluate("anything")
+
+def test__given_variable_evaluator__when_evaluate_unknown__then_raises_evaluation_error():
+    evaluator = VariableEvaluator(foo="bar")
+    with pytest.raises(EvaluationError):
+        evaluator.evaluate("unknown")
+
+def test__given_variable_evaluator__when_evaluate_known__then_returns_that_value():
+    evaluator = VariableEvaluator(foo="bar")
+    assert evaluator.evaluate("foo") == "bar"
+
+def test__given_large_variable_evaluator__when_evaluate_known__then_returns_that_value():
+    evaluator = VariableEvaluator(one=1, two="two", three=.14)
+    assert evaluator.evaluate("three") == .14
