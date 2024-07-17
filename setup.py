65f3049cfcbaeb85fd6626a12a8dfa7e31d79f29
Justin <justin.gray@reeldata.ai>
2024-07-16 23:20:49 -0300

    boilerplate

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="just_in_pyfig",
+    version="0.1.0",
+    packages=["just_in_pyfig"],
+    install_requires=[
+        "pydantic" # TODO be specific
+    ],
+)
