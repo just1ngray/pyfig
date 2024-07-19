3af42b485c6a0e659a710111de8159eb1ee42360
Justin <justin.gray@reeldata.ai>
2024-07-19 01:24:47 -0300

    string evaluator

--- pyfig/_eval/__init__.py
+++ pyfig/_eval/__init__.py
@@ -3,3 +3,4 @@ from .abstract_evaluator import AbstractEvaluator
 from .variable_evaluator import VariableEvaluator
 from .environment_evaluator import EnvironmentEvaluator
 from .python_evaluator import PythonEvaluator
+from .string_evaluator import StringEvaluator
