92fa8761bb1441e8d6cfe71cadf5becaca65329a
Justin <justin.gray@reeldata.ai>
2024-07-19 01:04:19 -0300

    python evaluator

--- pyfig/_eval/__init__.py
+++ pyfig/_eval/__init__.py
@@ -2,3 +2,4 @@ from .evaluate_conf import evaluate_conf
 from .abstract_evaluator import AbstractEvaluator
 from .variable_evaluator import VariableEvaluator
 from .environment_evaluator import EnvironmentEvaluator
+from .python_evaluator import PythonEvaluator
---
+++ pyfig/_eval/python_evaluator.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+from .abstract_evaluator import AbstractEvaluator
+
+
+class PythonEvaluator(AbstractEvaluator):
+    """
+    A template evaluator which evaluates a Python expression.
+    This is especially powerful when used with other nested templates.
+
+    It should be used with caution as it can execute arbitrary code.
+    """
+
+    def name(self) -> str:
+        return "py"
+
+    def evaluate(self, value: str) -> Any:
+        # pylint: disable=eval-used
+        return eval(value, {}, {})
