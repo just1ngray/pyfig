fba5eb0959fc52fdbac13e498ff03d48fa9fbab5
Justin <justin.gray@reeldata.ai>
2024-07-19 12:59:51 -0300

    sympy math evaluator

--- pyfig/_eval/__init__.py
+++ pyfig/_eval/__init__.py
@@ -4,3 +4,4 @@ from .environment_evaluator import EnvironmentEvaluator
 from .python_evaluator import PythonEvaluator
 from .string_evaluator import StringEvaluator
 from .cat_evaluator import CatEvaluator
+from .sympy_evaluator import SympyEvaluator
