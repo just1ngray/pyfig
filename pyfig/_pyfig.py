8edf652e62ea425c2090fb0925296f702471ff36
Justin Gray <just1ngray@outlook.com>
2025-06-28 15:26:23 -0300

    Validate default values when defining a Pyfig class (#30)
    Pyfig requires that all fields have a default value. If you try to define a Pyfig class without a default, you'll get an error.
    Now, if you provide a bad default value it will raise a ValidationError.
    
    This can be disabled by providing model_config = { "validate_default": False } in your Pyfig classes.

--- pyfig/_pyfig.py
+++ pyfig/_pyfig.py
@@ -1,6 +1,6 @@
 import json
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from pydantic_core import PydanticUndefined
 
 
@@ -12,6 +12,8 @@ class Pyfig(BaseModel):
     See: https://docs.pydantic.dev/latest/api/base_model/ for more information on validation, serialization, etc.
     """
 
+    model_config = ConfigDict(validate_default=True)
+
     @classmethod
     def __pydantic_init_subclass__(cls, **kwargs):
         """
@@ -20,6 +22,15 @@ class Pyfig(BaseModel):
         for name, field in cls.model_fields.items():
             if field.get_default() == PydanticUndefined:
                 raise TypeError(f"Field '{name}' of '{cls.__qualname__}' must have a default value")
+
+        # Construct the class once to see if the defaults are valid.
+        # 1. This makes defining a Pyfig class with bad defaults raise a ValidationError, rather than waiting for an
+        #    instance of the class to be constructed.
+        # 2. _pyfig_defaults_validated is set so we only validate the class's defaults once. This prevents issues with
+        #    Derived* classes with different model config
+        if not hasattr(cls, "_pyfig_defaults_validated"):
+            cls()
+            setattr(cls, "_pyfig_defaults_validated", True)
 
 
     def model_dump_dict(self) -> dict:
