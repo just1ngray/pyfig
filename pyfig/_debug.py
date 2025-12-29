c50f92ff2af4c5aca454c2d7d2eba15f2bf55a07
Justin Gray <just1ngray@outlook.com>
2025-12-28 23:58:43 -0400

    stop using static methods so they really aren't exposed

--- pyfig/_debug.py
+++ pyfig/_debug.py
@@ -18,45 +18,12 @@ class PyfigDebug(BaseModel):
 
         return super().__getattribute__(name)
 
-    @staticmethod
-    def _pyfig_debug_accesses(cfg: Union["PyfigDebug", list, dict]) -> Generator[Tuple[List[str], int], Any, None]:
-        accepted = (PyfigDebug, list, dict)
-
-        if isinstance(cfg, PyfigDebug):
-            for field_name, num_accessed in getattr(cfg, _ACCESS_COUNTER).items():
-                yield ([field_name], num_accessed)
-
-                value = super(BaseModel, cfg).__getattribute__(field_name)
-                if isinstance(value, accepted):
-                    for sub_paths, sub_num_accessed in PyfigDebug._pyfig_debug_accesses(value):
-                        yield ([field_name, *sub_paths], sub_num_accessed)
-            return
-
-        elif isinstance(cfg, list):
-            for i, item in enumerate(cfg):
-                if isinstance(item, accepted):
-                    for sub_paths, num in PyfigDebug._pyfig_debug_accesses(item):
-                        yield ([f"[{i}]", *sub_paths], num)
-
-        elif isinstance(cfg, dict):
-            for k, v in cfg.items():
-                if isinstance(v, accepted):
-                    for sub_paths, num in PyfigDebug._pyfig_debug_accesses(v):
-                        yield ([f"[{repr(k)}]", *sub_paths], num)
-
-        elif isinstance(cfg, accepted):
-            raise NotImplementedError()
-
-        else:
-            raise TypeError()
-
-
     def pyfig_debug_accesses(self) -> Generator[Tuple[str, int], Any, None]:
         """
         Recursive iterator over the fields and how frequently they've been accessed. Ordered to first yield
         shallower config paths before deeper ones.
         """
-        for path, num in PyfigDebug._pyfig_debug_accesses(self):
+        for path, num in _pyfig_debug_accesses(self):
             formatted_path = path[0]
             for p in path[1:]:
                 if p[0] == "[":
@@ -65,7 +32,6 @@ class PyfigDebug(BaseModel):
                     formatted_path += f".{p}"
 
             yield (formatted_path, num)
-
 
     def pyfig_debug_unused(self) -> Generator[str, Any, None]:
         """
@@ -99,46 +65,6 @@ class PyfigDebug(BaseModel):
         return super().__getattribute__(_ACCESS_COUNTER)[field]
 
     @staticmethod
-    def _wrap(cfg):
-        if isinstance(cfg, BaseModel):
-            debug_class = type(f"{cfg.__class__.__name__}PyfigDebug", (cfg.__class__, PyfigDebug), {})
-
-            new_instance = cfg.model_copy()
-            new_instance.__class__ = debug_class
-            setattr(new_instance, _ACCESS_COUNTER, { field: 0 for field in new_instance.__class__.model_fields })
-
-            for fieldname in new_instance.__class__.model_fields:
-                value = super(BaseModel, new_instance).__getattribute__(fieldname)
-                if isinstance(value, _ACCEPTED_INPUT_TYPES):
-                    setattr(new_instance, fieldname, PyfigDebug._wrap(value))
-
-            return new_instance
-
-        elif isinstance(cfg, list):
-            wrapped = []
-            for item in cfg:
-                if isinstance(item, _ACCEPTED_INPUT_TYPES):
-                    wrapped.append(PyfigDebug._wrap(item))
-                else:
-                    wrapped.append(item)
-            return wrapped
-
-        elif isinstance(cfg, dict):
-            wrapped = {}
-            for k, v in cfg.items():
-                if isinstance(v, _ACCEPTED_INPUT_TYPES):
-                    wrapped[k] = PyfigDebug._wrap(v)
-                else:
-                    wrapped[k] = v
-            return wrapped
-
-        elif isinstance(cfg, _ACCEPTED_INPUT_TYPES):
-            raise NotImplementedError()
-
-        else:
-            raise TypeError()
-
-    @staticmethod
     def wrap(cfg: T) -> T:
         """
         !! WARNING !! This feature is experimental and may not work properly for configs with untested edge cases.
@@ -162,4 +88,83 @@ class PyfigDebug(BaseModel):
             a subclass tree of cfg instance which should be usable in the same ways as the original `cfg` arg,
             but with additional tracking behaviour that allows you to later check how often each field is used.
         """
-        return PyfigDebug._wrap(cfg)
+        return _wrap(cfg)
+
+
+def _pyfig_debug_accesses(cfg: Union["PyfigDebug", list, dict]) -> Generator[Tuple[List[str], int], Any, None]:
+    """
+    Walks through a config recursively (incl. through selected types of collections) and yields a mapping
+    between config paths to the number of times each field has been accessed.
+    """
+    accepted = (PyfigDebug, list, dict)
+
+    if isinstance(cfg, PyfigDebug):
+        for field_name, num_accessed in getattr(cfg, _ACCESS_COUNTER).items():
+            yield ([field_name], num_accessed)
+
+            value = super(BaseModel, cfg).__getattribute__(field_name)
+            if isinstance(value, accepted):
+                for sub_paths, sub_num_accessed in _pyfig_debug_accesses(value):
+                    yield ([field_name, *sub_paths], sub_num_accessed)
+        return
+
+    elif isinstance(cfg, list):
+        for i, item in enumerate(cfg):
+            if isinstance(item, accepted):
+                for sub_paths, num in _pyfig_debug_accesses(item):
+                    yield ([f"[{i}]", *sub_paths], num)
+
+    elif isinstance(cfg, dict):
+        for k, v in cfg.items():
+            if isinstance(v, accepted):
+                for sub_paths, num in _pyfig_debug_accesses(v):
+                    yield ([f"[{repr(k)}]", *sub_paths], num)
+
+    elif isinstance(cfg, accepted):
+        raise NotImplementedError()
+
+    else:
+        raise TypeError()
+
+
+def _wrap(cfg):
+    """
+    Recursive conversion into the PyfigDebug type.
+    """
+    if isinstance(cfg, BaseModel):
+        debug_class = type(f"{cfg.__class__.__name__}PyfigDebug", (cfg.__class__, PyfigDebug), {})
+
+        new_instance = cfg.model_copy()
+        new_instance.__class__ = debug_class
+        setattr(new_instance, _ACCESS_COUNTER, { field: 0 for field in new_instance.__class__.model_fields })
+
+        for fieldname in new_instance.__class__.model_fields:
+            value = super(BaseModel, new_instance).__getattribute__(fieldname)
+            if isinstance(value, _ACCEPTED_INPUT_TYPES):
+                setattr(new_instance, fieldname, _wrap(value))
+
+        return new_instance
+
+    elif isinstance(cfg, list):
+        wrapped = []
+        for item in cfg:
+            if isinstance(item, _ACCEPTED_INPUT_TYPES):
+                wrapped.append(_wrap(item))
+            else:
+                wrapped.append(item)
+        return wrapped
+
+    elif isinstance(cfg, dict):
+        wrapped = {}
+        for k, v in cfg.items():
+            if isinstance(v, _ACCEPTED_INPUT_TYPES):
+                wrapped[k] = _wrap(v)
+            else:
+                wrapped[k] = v
+        return wrapped
+
+    elif isinstance(cfg, _ACCEPTED_INPUT_TYPES):
+        raise NotImplementedError()
+
+    else:
+        raise TypeError()
