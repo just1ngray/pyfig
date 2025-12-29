13523f097f273ea89f9f2ac540e9daa4d004c814
Justin Gray <just1ngray@outlook.com>
2025-12-28 20:25:33 -0400

    simple implementation is working
    next: lists, dicts, and possibly other collections?

--- pyfig/_debug.py
+++ pyfig/_debug.py
@@ -1,10 +1,9 @@
-from typing import Any, TypeVar
+from typing import Any, Generator, TypeVar, List, Tuple
 
 from pydantic import BaseModel
 
 
 T = TypeVar("T", bound=BaseModel)
-
 
 _ACCESS_COUNTER = "_pyfig_debug_access_counter"
 
@@ -17,7 +16,11 @@ class PyfigDebug(BaseModel):
 
         return super().__getattribute__(name)
 
-    def pyfig_debug_accessed(self):
+    def pyfig_debug_accesses(self) -> Generator[Tuple[str, int], Any, None]:
+        """
+        Recursive iterator over the fields and how frequently they've been accessed. Ordered to first yield
+        shallower config paths before deeper ones.
+        """
         field_name: str
         num_accessed: int
         for field_name, num_accessed in super().__getattribute__(_ACCESS_COUNTER).items():
@@ -25,60 +28,75 @@ class PyfigDebug(BaseModel):
 
             value = super().__getattribute__(field_name)
             if isinstance(value, PyfigDebug):
-                for sub_field_name, sub_num_accessed in value.pyfig_debug_accessed():
+                for sub_field_name, sub_num_accessed in value.pyfig_debug_accesses():
                     yield (f"{field_name}.{sub_field_name}", sub_num_accessed)
 
+    def pyfig_debug_unused(self) -> Generator[str, Any, None]:
+        """
+        Reports the highest level path(s) that have not been accessed.
 
-def pyfig_debug(cfg: T) -> T:
-    debug_class = type(f"{cfg.__class__.__name__}PyfigDebug", (cfg.__class__, PyfigDebug), {})
+        Note: deeper paths cannot be accessed without their parent being accessed at least once!
+        """
+        reported: List[str] = []
 
-    new_instance = cfg.model_copy()
-    new_instance.__class__ = debug_class
-    setattr(new_instance, _ACCESS_COUNTER, {})
+        for path, n in self.pyfig_debug_accesses():
+            if n > 0:
+                continue
 
-    for fieldname in new_instance.__class__.model_fields:
-        value = getattr(new_instance, fieldname)
+            if not any(path.startswith(r) for r in reported):
+                reported.append(path)
+                yield path
 
-        if isinstance(value, BaseModel):
-            setattr(new_instance, fieldname, pyfig_debug(value))
+    def pyfig_debug_field_accesses(self, field: str) -> int:
+        """
+        Gets the number of times a specific field has been accessed.
 
-    # start tracking now
-    getattr(new_instance, _ACCESS_COUNTER).update({ field: 0 for field in new_instance.__class__.model_fields })
+        Args:
+            field: the field name to check
 
-    return new_instance
+        Returns:
+            the number of times that the given field has been accessed
 
+        Raises:
+            KeyError if the provided field name is not tracked
+        """
+        return super().__getattribute__(_ACCESS_COUNTER)[field]
 
-class Nested(BaseModel):
-    baz: str = "baz"
+    @staticmethod
+    def wrap(cfg: T) -> T:
+        """
+        Copies a Pyfig and injects behaviour to track the number of times each field is accessed.
 
-class Example(BaseModel):
-    foo: str = "foo"
-    bar: Nested = Nested()
+        Usage:
+            >>> config: Pyfig = load_configuration()
+            >>> config = PyfigDebug.wrap(config) if os.environ.get("DEBUG") else config
+            >>> # run your app normally
+            >>> # ...
+            >>> # check how often each field has or hasn't been used
+            >>> if isinstance(config, PyfigDebug):
+            ...     for path, n in config.pyfig_debug_accesses():
+            ...         print(f"{path} accessed {n} times")
 
+        Args:
+            cfg: the config to debug
 
-ex = Example()
-print(type(ex))
-print(ex)
-print(ex.foo)
-print(ex.model_dump_json(indent=4))
+        Returns:
+            a subclass tree of cfg instance which should be usable in the same ways as the original `cfg` arg,
+            but with additional tracking behaviour that allows you to later check how often each field is used.
+        """
+        debug_class = type(f"{cfg.__class__.__name__}PyfigDebug", (cfg.__class__, PyfigDebug), {})
 
-print("---")
+        new_instance = cfg.model_copy()
+        new_instance.__class__ = debug_class
+        setattr(new_instance, _ACCESS_COUNTER, {})
 
-ex_debug = pyfig_debug(ex)
-print(type(ex_debug))
-print(list(ex_debug.pyfig_debug_accessed()))
-print(ex_debug.foo)
-print(list(ex_debug.pyfig_debug_accessed()))
-print(ex_debug.foo)
-print(list(ex_debug.pyfig_debug_accessed()))
-print(ex_debug.model_dump_json(indent=4))
+        for fieldname in new_instance.__class__.model_fields:
+            value = getattr(new_instance, fieldname)
 
-print("---")
+            if isinstance(value, BaseModel):
+                setattr(new_instance, fieldname, PyfigDebug.wrap(value))
 
-print(type(ex))
-print(ex)
-print(ex.foo)
-print(ex.model_dump_json(indent=4))
+        # start tracking now
+        getattr(new_instance, _ACCESS_COUNTER).update({ field: 0 for field in new_instance.__class__.model_fields })
 
-print("---")
-print(list(ex_debug.pyfig_debug_accessed()))
+        return new_instance
---
+++ pyfig/_debug_test.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+
+from ._pyfig import Pyfig
+from ._debug import PyfigDebug
+
+
+def test__given_simple_model__when_pyfig_debug_wrap__then_tracks_access():
+    class SimpleConfig(Pyfig):
+        foo: str = "foo"
+        bar: str = "bar"
+
+    original = SimpleConfig()
+    tracking = PyfigDebug.wrap(original)
+    assert isinstance(tracking, SimpleConfig)
+
+    print(original.foo)
+    print(tracking.bar)
+
+    assert isinstance(tracking, PyfigDebug)
+    assert tracking.pyfig_debug_field_accesses("foo") == 0
+    assert tracking.pyfig_debug_field_accesses("bar") == 1
+    print(tracking.bar)
+    assert tracking.pyfig_debug_field_accesses("bar") == 2
+    assert list(tracking.pyfig_debug_unused()) == ["foo"]
+
+
+def test__given_nested_model__when_pyfig_debug_wrap__then_tracks_access():
+    class LoggingConfig(Pyfig):
+        level: str = "INFO"
+        stdout: bool = False
+        stderr: bool = True
+
+    class DeadModuleConfig(Pyfig):
+        unused: bool = True
+
+    class SomeServiceConfig(Pyfig):
+        enabled: bool = True
+        interval_seconds: float = 3.14
+
+    class AnotherServiceConfig(Pyfig):
+        enabled: bool = False
+        path: Path = Path("path.txt")
+
+    class ServicesConfig(Pyfig):
+        some_service: SomeServiceConfig = SomeServiceConfig()
+        another_service: AnotherServiceConfig = AnotherServiceConfig()
+
+    class RootConfig(Pyfig):
+        logging: LoggingConfig = LoggingConfig()
+        services: ServicesConfig = ServicesConfig()
+        dead: DeadModuleConfig = DeadModuleConfig()
+
+    def main(cfg: RootConfig):
+        print(f"Setting up {cfg.logging.level} level logging...")
+        if cfg.services.some_service.enabled:
+            print(f"Running SomeService every {cfg.services.some_service.interval_seconds} seconds...")
+        if cfg.services.another_service.enabled:
+            print(f"Running AnotherService against {cfg.services.another_service.path.as_posix()}")
+
+    root = RootConfig()
+    # it does not matter if the other configs are used elsewhere
+    main(root)
+
+    tracked = PyfigDebug.wrap(root)
+    main(tracked)
+
+    # it does not matter if the other configs are used elsewhere
+    main(root)
+
+    if isinstance(tracked, PyfigDebug):
+        assert set(tracked.pyfig_debug_unused()) == {
+            "dead",
+            "logging.stdout",
+            "logging.stderr",
+            "services.another_service.path",
+        }
+        assert dict(tracked.pyfig_debug_accesses()) == {
+            "logging": 1,
+            "logging.level": 1,
+            "logging.stdout": 0,
+            "logging.stderr": 0,
+            "services": 3,
+            "services.some_service": 2,
+            "services.some_service.enabled": 1,
+            "services.some_service.interval_seconds": 1,
+            "services.another_service": 1,
+            "services.another_service.enabled": 1,
+            "services.another_service.path": 0,
+            "dead": 0,
+            "dead.unused": 0,
+        }
+    else:
+        raise RuntimeError("It must be an instance. The isinstance check just demonstrates how to "
+                           "incorporate this nicely into your 'main' with good typing")
