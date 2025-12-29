6b1ac540c257b0e95316e9e781e728a7e9c30541
Justin Gray <just1ngray@outlook.com>
2025-12-29 15:41:34 -0400

    Prep v0.3.0 (#38)
    * prepare for v0.3.0
    
    * update the tiny description

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="jpyfig",
-    version="0.2.1",
+    version="0.3.0",
     author="Justin Gray",
     author_email="just1ngray@outlook.com",
     url="https://github.com/just1ngray/pyfig",
-    description=" A simple, yet capable configuration library for Python",
+    description="Configuration library built around pydantic",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     license="MIT",
