14ead10bf5ea3a3df799967a83e556c9ba925c65
Justin Gray <just1ngray@outlook.com>
2026-07-25 18:04:22 -0300

    keep old tests for backwards compat only

--- pyfig/_eval/environment_evaluator_test.py
+++ pyfig/_eval/environment_evaluator_test.py
@@ -54,3 +54,24 @@ def test_given_environment_evaluator_with_default_and_defaults_when_evaluate_the
     assert evaluator.evaluate(key) == value
     assert evaluator.evaluate("EXPLICIT") == defaults["EXPLICIT"]
     assert evaluator.evaluate("ANYTHING_ELSE") == default
+
+
+### OLD TESTS - kept only to ensure backwards compatibility       ###
+### subject for future removal as they add no additional coverage ###
+
+def test__given_default_environment_evaluator__when_value_is_not_in_env__then_raises_key_error():
+    evaluator = EnvironmentEvaluator()
+    with pytest.raises(KeyError):
+        evaluator.evaluate("this-environment-variable-shouldn't-exist")
+
+def test__given_environment_evaluator_with_default__when_value_is_not_in_env__then_returns_default():
+    evaluator = EnvironmentEvaluator(default=13)
+    assert evaluator.evaluate("this-environment-variable-shouldn't-exist") == 13
+
+def test__given_environment_evaluator__when_env_value_is_set__then_returns_that_value(monkeypatch: pytest.MonkeyPatch):
+    env = "PYFIG_TEST"
+    val = "test-value"
+    monkeypatch.setenv(env, val)
+    evaluator = EnvironmentEvaluator()
+
+    assert evaluator.evaluate(env) == val
